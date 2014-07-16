7139e05bdd44d8b7dacac3bf2cf3b131e4104b11
Ethan Jennings <ethanhjennings@github.com>
2014-07-15 23:01:11 -0700

    Added cells and w

--- draw.js
+++ draw.js
@@ -5,45 +5,108 @@ $(document).ready(function () {
     var width = canvas.width;
     var height = canvas.height;
 
-    var x = 0;
+    grid = new Grid(width,height,3);
+    var colorData = [];
+    for (var x = 0; x < grid.gridWidth; x++) {
+        colorData.push([]);
+        for (var y = 0; y < grid.gridHeight; y++) {
+            colorData[x].push("rgba(0,0,0,0.0)");
+        }
+    }
+    var lastMousePos;
+
+    function getXY(event) {
+        return {x:event.clientX-canvas.offsetLeft-3, y:event.clientY-canvas.offsetTop+5};
+    }
+    var mouseDown = false;
+    $(canvas).mousedown(function(event) {
+        mouseDown = true;
+        lastMousePos = getXY(event);
+        console.log("mouse down " + event);
+    });
+    $("body").mousemove(function(mousemoveEvent) {
+        if (mouseDown) {
+            var newPoints = grid.convertLineToGridPoints(lastMousePos,getXY(mousemoveEvent));
+            $.each(newPoints, function(i,point) {
+                colorData[point.x][point.y] = "white";
+            });
+            lastMousePos = getXY(mousemoveEvent);
+        }
+        console.log("mouse move " + event);
+    });
+
+    $(canvas).mouseup(function (event) {
+        mouseDown = false;
+        lastMousePos = getXY(event);
+        console.log("mouse up " + event);
+    });
 
     // makes the lines crisper
     context.translate(-0.5,-0.5);
 
-    (function runLoop() {
-        window.requestAnimationFrame(runLoop);
-        context.clearRect(0,0,canvas.width,canvas.height);
+    function getWrappedPos(pos) {
+        var result = pos;
+        while (result.x < 0)
+            result.x += grid.gridWidth;
+        while (result.y < 0)
+            result.y += grid.gridWidth;
+        while (result.x >= grid.gridWidth)
+            result.x -= grid.gridWidth;
+        while (result.y >= grid.gridHeight)
+            result.y -= grid.gridHeight;
+        return result;
+    }
 
-        var CELL_SIZE = 10;
-        var gridWidth = width/CELL_SIZE;
-        var gridHeight = height/CELL_SIZE;
+    function result(pos) {
+        pos = getWrappedPos(pos);
+        if ((colorData[pos.x][pos.y]) == "white")
+            return true;
+        else
+            return false;
+    }
 
-        context.lineWidth="1";
-        context.strokeStyle="rgba(255,255,255,0.2)";
-
-        for (var x = 0; x < gridWidth; x++) {
-            context.beginPath();
-            context.moveTo(x*CELL_SIZE,0);
-            context.lineTo(x*CELL_SIZE,height);
-            context.stroke();
-        }
-    
-        for (var y = 0; y < gridHeight; y++) {
-            context.beginPath();
-            context.moveTo(0,y*CELL_SIZE);
-            context.lineTo(width,y*CELL_SIZE);
-            context.stroke();
-        }
-
-        context.fillStyle = "rgba(255,255,255,1.0)";
-
-        for (var y = 0; y < gridHeight; y++) {
-            for (var x = 0; x < gridWidth; x++) {
-                if (Math.random() > 0.5) {
-                    context.fillRect(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE,CELL_SIZE);
+    function countNeighbors(pos) {
+        var neighborCount = 0;
+        for (var x = -1; x <= 1; x++) {
+            for (var y = -1; y <= 1; y++) {
+                if (!(x == 0 && y == 0) && result({x:pos.x+x,y:pos.y+y}) == true) {
+                    neighborCount += 1;
                 }
             }
         }
+        return neighborCount;
+    }
+
+    counter = 0;
+
+    (function runLoop() {
+        window.requestAnimationFrame(runLoop);
+
+        if (counter >= 0) {
+            //for (var x = 0; x < grid.gridWidth; x++) {
+                //newColorData.push([]);
+            //    for (var y = 0; y < grid.gridHeight; y++) {
+            //        colorData[x][y] = (Math.random()>0.9 ? "white" : "rgba(0,0,0,0.0)");
+                    //var count = countNeighbors({x:x,y:y});
+                    //if (count == 3)
+                    //    newColorData[x].push("white");
+                    //else if (count < 2)
+                    //    newColorData[x].push("rgba(0,0,0,0.0)");
+                    //else if (count > 3)
+                    //    newColorData[x].push("rgba(0,0,0,0.0)");
+                    //else
+                    //    newColorData[x].push(colorData[x][y]);
+            //    }
+            //}
+            //colorData = newColorData;
+        }
+        if (counter >= 0) {
+            
+            context.clearRect(0,0,canvas.width,canvas.height);
+            grid.draw(context,colorData);
+            counter = 0;
+        }
+        counter++;
 
     })();
 });
--- grid.js
+++ grid.js
@@ -1,44 +1,77 @@
-function grid(width,height,cellsize) {
-	this.cellSize = cellSize;
-	this.gridWidth = width/CELL_SIZE;
-    this.gridHeight = height/CELL_SIZE;
+function Grid(width,height,cellsize) {
+	this.cellSize = cellsize;
+	this.gridWidth = width/this.cellSize;
+    this.gridHeight = height/this.cellSize;
     this.width = width;
     this.height = height;
     this.decimalCellWidth = this.width/this.gridWidth;
-    this.decimalCellHeight = this.height/this.gridWidth;
+    this.decimalCellHeight = this.height/this.gridHeight;
 }
 
-grid.prototype.draw = function(context) {
-        context.lineWidth="1";
-        context.strokeStyle="rgba(255,255,255,0.2)";
+Grid.prototype.convertPointToGridPoint = function(point) {
+    return {x:Math.floor(point.x/this.decimalCellWidth), 
+            y:Math.floor(point.y/this.decimalCellHeight)};
+}
+
+Grid.prototype.convertLineToGridPoints = function(startPoint,endPoint) {
+    // use Bresenham's algorithm
+    startPoint = this.convertPointToGridPoint(startPoint);
+    endPoint = this.convertPointToGridPoint(endPoint);
+    var x0 = startPoint.x;
+    var y0 = startPoint.y;
+    var x1 = endPoint.x;
+    var y1 = endPoint.y;
+    var dx = Math.abs(x1-x0);
+    var dy = Math.abs(y1-y0);
+    var sx = (x0 < x1) ? 1 : -1;
+    var sy = (y0 < y1) ? 1 : -1;
+    var err = dx-dy;
+
+    var resultArray = [];
+
+    while(true){
+        var point ={x:x0, y:y0};
+        point.x = Math.min(this.gridWidth,Math.max(0,point.x));
+        point.y = Math.min(this.gridHeight,Math.max(0,point.y));
+        resultArray.push({x:point.x,y:point.y});
+
+        if ((x0==x1) && (y0==y1)) break;
+        var e2 = 2*err;
+        if (e2 >-dy){ err -= dy; x0  += sx; }
+        if (e2 < dx){ err += dx; y0  += sy; }
+    }
+
+    return resultArray;
+}
 
 
+Grid.prototype.draw = function(context,colorData) {
+    context.lineWidth="1";
+    context.strokeStyle="rgba(255,255,255,0.2)";
 
+    for (var x = 0; x < this.gridWidth; x++) {
+        var xPos = Math.floor(x*this.decimalCellWidth);
+
+        context.beginPath();
+        context.moveTo(xPos,0);
+        context.lineTo(xPos,this.height);
+        context.stroke();
+    }
+
+    for (var y = 0; y < this.gridHeight; y++) {
+        var yPos = Math.floor(y*this.decimalCellHeight);
+
+        context.beginPath();
+        context.moveTo(0,yPos);
+        context.lineTo(this.width,yPos);
+        context.stroke();
+    }
+
+    for (var y = 0; y < this.gridHeight; y++) {
         for (var x = 0; x < this.gridWidth; x++) {
-            var xPos = (int)(x*this.decimalCellWidth);
-
-            context.beginPath();
-            context.moveTo(xPos,0);
-            context.lineTo(xPos,height);
-            context.stroke();
+            context.strokeWidth = "0";
+            context.fillStyle = colorData[x][y];
+            context.fillRect(x*this.cellSize-0.5,y*this.cellSize-0.5,this.cellSize+1.0,this.cellSize+1.0);
         }
-    
-        for (var y = 0; y < this.gridHeight; y++) {
-            var yPos = (int)(y*this.decimalCellHeight);
-
-            context.beginPath();
-            context.moveTo(0,y*CELL_SIZE);
-            context.lineTo(width,y*CELL_SIZE);
-            context.stroke();
-        }
-
-        context.fillStyle = "rgba(255,255,255,1.0)";
-
-        for (var y = 0; y < gridHeight; y++) {
-            for (var x = 0; x < gridWidth; x++) {
-                if (Math.random() > 0.5) {
-                    context.fillRect(x*CELL_SIZE,y*CELL_SIZE,CELL_SIZE,CELL_SIZE);
-                }
-            }
-        }
+    }
 };
